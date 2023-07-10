67d2f4be0e8246a76d67aa2f1fdcc9965f4ddb0b
skywang-2070 <727854256@qq.com>
2023-07-10 23:56:52 +0800

    [chg][normal]1.添加检测pawn之间直线距离的行为树服务；2.在编辑器中给行为树添加测距服务，添加黑板作为装饰器

--- ActionRoguelike/Source/ActionRoguelike/Private/AI/SBTService_CheckAttackRange.cpp
+++ ActionRoguelike/Source/ActionRoguelike/Private/AI/SBTService_CheckAttackRange.cpp
@@ -3,7 +3,33 @@
 
 #include "AI/SBTService_CheckAttackRange.h"
 
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+//作用：检测AI对象与目标之间的距离
+
 void USBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	//获取黑板指针
+	UBlackboardComponent* blackboard = OwnerComp.GetBlackboardComponent();
+	if(ensure(blackboard))
+	{
+		AActor* target = Cast<AActor>(blackboard->GetValueAsObject("TargetActor"));
+		if(target)
+		{
+			AAIController* controller = OwnerComp.GetAIOwner();
+			if(ensure(controller))
+			{
+				APawn* aiPawn = controller->GetPawn();
+				if(ensure(aiPawn))
+				{
+					const float distanceTo = FVector::Distance(target->GetActorLocation(), aiPawn->GetActorLocation());
+					bool bWithinRange = distanceTo < 2000.f;
+					blackboard->SetValueAsBool(AttackRangeKey.SelectedKeyName, bWithinRange);//在编辑器中设置黑板变量
+				}
+			}
+		}
+	}
 }
--- ActionRoguelike/Source/ActionRoguelike/Public/AI/SBTService_CheckAttackRange.h
+++ ActionRoguelike/Source/ActionRoguelike/Public/AI/SBTService_CheckAttackRange.h
@@ -7,7 +7,7 @@
 #include "SBTService_CheckAttackRange.generated.h"
 
 /**
- * 
+ * AI服务，检测AI对象到目标是否在指定距离内
  */
 UCLASS()
 class ACTIONROGUELIKE_API USBTService_CheckAttackRange : public UBTService
@@ -15,6 +15,9 @@ class ACTIONROGUELIKE_API USBTService_CheckAttackRange : public UBTService
 	GENERATED_BODY()
 
 protected:
+	//在行为树-服务节点上显示选变量的下拉列表，这样可以在界面上选择要读取的黑板变量，不必硬编码
+	UPROPERTY(EditAnywhere, Category = "AI_Set")
+	FBlackboardKeySelector AttackRangeKey;
+	
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
-
 };
